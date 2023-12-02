5baaac0addaa97ac7aed5e4da2cbcfb6662ed6b5
jaronsander <jar.sand.on@gmail.com>
2023-12-02 08:48:00 -0800

    init

---
+++ app/api/chat/route.ts
@@ -0,0 +1,76 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
+
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { BytesOutputParser } from "langchain/schema/output_parser";
+import { PromptTemplate } from "langchain/prompts";
+
+export const runtime = "edge";
+
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`;
+};
+
+const TEMPLATE = `You are a sales manager called Notitia for a company called inForm. inForm is a growth agency that offers a holistic approach to growth engineering. They help companies grow by building and optimizing their growth stack, and by providing them with a growth team. They provide value by alleviating the following pain points:
+Pain 1: just reporting metrics, lack of insights coming from reports. Just “good” or “bad”, lack of clarity as to “why”
+Value 1: insightful reporting on relationships between different parts of the funnel. Seek clarity on “slices” of funnel
+Pain 2: volatile sales, no seeming predictability or pattern to lead gen and sales.
+Value 2: Building resilience through implementing experimentation frameworks that generate insight and allow you to see what is working and what is now
+Pain 3: Sales funnel is inefficient, tire kickers wasting sales reps time, lots of follow up required by sales rep, lots of back and forth required. Sales teams time is not being used effectively
+Value 3: Leveraging modern technology to optimize processes like large language models and statistical models. Ai pre qualification, follow up clarity, sales rep action recommendations.
+
+Current conversation:
+{chat_history}
+
+User: {input}
+AI:`;
+
+/**
+ * This handler initializes and calls a simple chain with a prompt,
+ * chat model, and output parser. See the docs for more information:
+ *
+ * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
+ */
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const messages = body.messages ?? [];
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+    const currentMessageContent = messages[messages.length - 1].content;
+    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+    /**
+     * You can also try e.g.:
+     *
+     * import { ChatAnthropic } from "langchain/chat_models/anthropic";
+     * const model = new ChatAnthropic({});
+     *
+     * See a full list of supported models at:
+     * https://js.langchain.com/docs/modules/model_io/models/
+     */
+    const model = new ChatOpenAI({
+      temperature: 0.8,
+    });
+    /**
+     * Chat models stream message chunks rather than bytes, so this
+     * output parser handles serialization and byte-encoding.
+     */
+    const outputParser = new BytesOutputParser();
+
+    /**
+     * Can also initialize as:
+     *
+     * import { RunnableSequence } from "langchain/schema/runnable";
+     * const chain = RunnableSequence.from([prompt, model, outputParser]);
+     */
+    const chain = prompt.pipe(model).pipe(outputParser);
+
+    const stream = await chain.stream({
+      chat_history: formattedPreviousMessages.join("\n"),
+      input: currentMessageContent,
+    });
+
+    return new StreamingTextResponse(stream);
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+}
